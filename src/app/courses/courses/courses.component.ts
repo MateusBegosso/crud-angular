6ad8162458046cf030aae657e962d0ddb7faf68a
Mateus Begosso <mateusbegosso@gmail.com>
2024-01-11 21:42:39 -0300

    feat: add material table and course interface

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-
+  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'Front-end' }];
+  displayedColumns: string[] = ['name', 'category'];
 }
