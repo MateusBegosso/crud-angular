d5ad9a7827e63ded0b0539b1a9b60b80d58ec21c
Mateus Begosso <mateusbegosso@gmail.com>
2024-01-16 16:58:15 -0300

    feat: add app material module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { Course } from '../model/course';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss',
 })
