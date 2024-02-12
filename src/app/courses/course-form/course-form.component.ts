7c3fb98dbbfbd0097c32a97381541361d37d9e26
Mateus Begosso <mateusbegosso@gmail.com>
2024-02-12 19:56:34 -0300

    feat: Create Course (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,7 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { error } from 'node:console';
 
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 import { CoursesService } from '../services/courses.service';
@@ -19,7 +19,8 @@ export class CourseFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = formBuilder.group({
       name: [null],
@@ -29,12 +30,19 @@ export class CourseFormComponent {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      (data) => console.log(data),
+      (data) => this.onSuccess(),
       (error) => this.onError()
     );
   }
 
-  onCancel() {}
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
+  }
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
